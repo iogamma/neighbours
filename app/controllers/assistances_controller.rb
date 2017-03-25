affb8ebf281ea16f69f93e1452ab0e9026ce6062
99hsueh <99hsueh@gmail.com>
2017-03-25 23:34:41 +0000

    index page for assistances

--- app/controllers/assistances_controller.rb
+++ app/controllers/assistances_controller.rb
@@ -3,10 +3,13 @@ class AssistancesController < ApplicationController
   def index
     @assistances = Assistance.all.order(created_at: :desc)
     @assistance_new = Assistance.new
+    @neighbourhood = Neighbourhood.find params[:neighbourhood_id]
+
   end
 
   def show
     @assistance = Assistance.find params[:assistance_id]
+    @neighbourhood = Neighbourhood.find params[:neighbourhood_id]
   end
 
 end
