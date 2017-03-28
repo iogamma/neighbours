42f324515be6fc6e441c8b8129e493edcb3339ca
Jshen123 <joeshen123@gmail.com>
2017-03-28 19:18:46 +0000

    Added form submit function for events view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,27 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @neighbourhood = Neighbourhood.find params[:neighbourhood_id]
+    @event = Event.create(event_params)
+    @event.user_id = current_user.id
+    @event.neighbourhood_id = params[:neighbourhood_id]
+
+    if @event.save
+      redirect_to [@neighbourhood, :events], notice: 'Event created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :title,
+      :description,
+      :date
+    )
+  end
+
 end
