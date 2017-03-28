f7af62f8c1af9598d8821250cfb027c9958070d2
Jshen123 <joeshen123@gmail.com>
2017-03-28 22:11:59 +0000

    Added notification label for dashboard feed

--- app/helpers/neighbourhood_helper.rb
+++ app/helpers/neighbourhood_helper.rb
@@ -3,20 +3,25 @@ module NeighbourhoodHelper
   def feed_link (table, feed)
     case table
       when 'notices'
-        @link = neighbourhood_notice_path(feed.neighbourhood_id, feed)
+        neighbourhood_notice_path(feed.neighbourhood_id, feed)
       when 'assistances'
-        @link = neighbourhood_assistance_path(feed.neighbourhood_id, feed)
+        neighbourhood_assistance_path(feed.neighbourhood_id, feed)
       else
-        @link = neighbourhood_path
+        neighbourhood_path
     end
   end
 
-  def array_helper (array, key)
-    new_array = Array.new
-    array.each do |a|
-      new_array << a.key
+  def select_notification (table)
+    case table
+      when 'notices'
+        content_tag(:span, "#{table.titleize.singularize}", class: 'label label-default')
+      when 'assistances'
+        content_tag(:span, "#{table.titleize.singularize}", class: 'label label-primary')
     end
-    new_array
+  end
+
+  def find_user (user_id)
+    User.find user_id
   end
 
 end
