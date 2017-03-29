cee853b1b0df8c3d3d7c325fe2d2c45545bce2a8
Jshen123 <joeshen123@gmail.com>
2017-03-29 18:49:48 +0000

    Resolved conflicts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,23 +1,36 @@
 class EventsController < ApplicationController
   
   def show
+    @neighbourhood = Neighbourhood.find users_building.neighbourhood_id
     @event = Event.find params[:id]
   end
 
   def new
+    
     @event = Event.new
   end
 
   def create
-    @neighbourhood = Neighbourhood.find params[:neighbourhood_id]
+    user_neighbourhood_id = users_building.neighbourhood_id
+    @neighbourhood = Neighbourhood.find user_neighbourhood_id
     @event = Event.create(event_params)
     @event.user_id = current_user.id
-    @event.neighbourhood_id = params[:neighbourhood_id]
+    @event.neighbourhood_id = user_neighbourhood_id
 
     if @event.save
-      redirect_to [@neighbourhood, :events], notice: 'Event created'
+      redirect_to [@neighbourhood, @event], notice: 'Event created'
     else
       render :new
+    end
+  end
+  
+  def destroy
+    @neighbourhood = Neighbourhood.find users_building.neighbourhood_id
+    @assistance = Event.find params[:id]
+    @assistance.destroy
+    respond_to do |format|
+      format.html { redirect_to [@neighbourhood], notice: 'event is successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
@@ -27,7 +40,9 @@ class EventsController < ApplicationController
     params.require(:event).permit(
       :title,
       :description,
-      :date
+      :location,
+      :date,
+      :image
     )
   end
 
--- app/controllers/neighbourhoods_controller.rb
+++ app/controllers/neighbourhoods_controller.rb
@@ -1,18 +1,16 @@
 class NeighbourhoodsController < ApplicationController
   def index
-
     render 'index'
   end
 
-  def show  
-    @notices = Notice.where(neighbourhood_id: params[:id])
-    @assistances = Assistance.where(neighbourhood_id: params[:id])
-    @feeds = (@assistances + @notices).sort_by(&:created_at).reverse
 
-    @polls = Poll.where(neighbourhood_id: params[:id]).sort_by(&:created_at).reverse
-
-    @events = Event.where(neighbourhood_id: params[:id]).sort_by(&:created_at).reverse
-    
-    @neighbourhood = Neighbourhood.find params[:id]
+  def show
+    user_neighbourhood_id = users_building.neighbourhood_id
+    @notices = Notice.where(neighbourhood_id: user_neighbourhood_id)
+    @assistances = Assistance.where(neighbourhood_id: user_neighbourhood_id)
+    @feeds = Kaminari.paginate_array((@assistances + @notices).sort_by(&:created_at).reverse).page(params[:page]).per(10)
+    @polls = Poll.where(neighbourhood_id: user_neighbourhood_id).sort_by(&:created_at).reverse
+    @events = Event.where(neighbourhood_id: user_neighbourhood_id).sort_by(&:created_at).reverse
+    @neighbourhood = Neighbourhood.find(user_neighbourhood_id)
   end
 end
