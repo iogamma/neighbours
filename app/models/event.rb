cd8a64e4e8a9492673f3e615ce7e533d54cc15b8
99hsueh <99hsueh@gmail.com>
2017-04-01 22:00:01 +0000

    resolve conflict

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,13 +6,10 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :neighbourhood
 
-<<<<<<< HEAD
   has_many :event_comments, dependent: :delete_all
   has_many :attendees, dependent: :delete_all
-=======
   def crop_image
     image.recreate_versions! if crop_x.present?
   end
->>>>>>> 51c73a0fdf480b98e8bd2f1158af758d640a29b7
 
 end
