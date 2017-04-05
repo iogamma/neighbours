9dbfcd1876e155c535a59404f9d5a61255c75dfc
99hsueh <99hsueh@gmail.com>
2017-04-05 18:37:41 +0000

    added regex to fit more options for youtube url submission

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,11 +2,10 @@ class Video < ApplicationRecord
 
   belongs_to :meeting
 
-  # validates :youtube_url => {:message => "Please include full url." }
+  validates :location, presence: true, format: {with: /(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/, message: "url: please include full URL"}
 
-  private
 
-  def youtube_url
-    self.location.start_with?("https://youtu.be", "https://www.youtube.com/embed")
-  end
+  # def youtube_url(self)
+  #   self.location.start_with?("https://youtu.be", "https://www.youtube.com/embed")
+  # end
 end
